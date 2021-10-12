6fb96e7347034620e94fda26fc8f84036106f933
AlexShh <endlessair@mail.ru>
2021-10-12 22:55:26 +0300

    adds hw #1 v2

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("HW of lecturer");
+		
+		
+		System.out.println("HW is done v2");
 
 	}
 
